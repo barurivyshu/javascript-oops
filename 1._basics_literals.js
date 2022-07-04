50c6b316ca4c50d1e10cfe6ac2248c632332d7d7
barurivyshu <108330189+barurivyshu@users.noreply.github.com>
2022-07-04 12:42:51 +0530

    Add files via upload

---
+++ 1._basics_literals.js
@@ -0,0 +1,31 @@
+//const s1 = 'Hello';
+//console.log(typeof s1);
+
+//const s2 = new String('Hello);
+//console.log(typeof s2);
+
+//console.log(window);
+//alert(1);
+//console.log(navigator.appVersion);
+
+//Object Literal
+const book1 = {
+    title: 'Book One',
+    author: 'John Doe',
+    year:'2013',
+    getSummary:function() {
+        return'${this.title} was written by $ {this.author} in ${this.year}';
+    }
+};
+
+const book2 = {
+    title: 'Book Two',
+    author: 'Jane Doe',
+    year:'2016',
+    getSummary:function() {
+        return'${this.title} was written by $ {this.author} in ${this.year}';
+    }
+};
+//console.log(book1.getSummary());
+//console.log(object.values(book2));
+//console.log(object.keys (book2));
